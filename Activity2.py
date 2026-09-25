08c9dd75976f349c00f33dd153c218ee4781e9fc
MohamedAli714-glitch <nehal.shahtout2010@gmail.com>
2026-09-25 21:33:11 +0300

    40000000000000000000

---
+++ Activity2.py
@@ -0,0 +1,17 @@
+name="Mohamed"
+
+age=11
+
+weight=43.3
+
+is_student=True
+
+print("my name is", name, "and my age is", age, "and my weight is", weight, "and I am a student:", is_student)
+
+print("type of weight:", type(weight))
+
+print("type of age:", type(age))
+
+weight=int(weight)
+
+print("type of weight after conversion:", type(weight))
