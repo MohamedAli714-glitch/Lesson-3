e1f1b55634b0936b57c49daafe5e82651fff757f
MohamedAli714-glitch <nehal.shahtout2010@gmail.com>
2026-09-26 09:03:01 +0300

    I added extra stuff to the homework file. Please review and let me know if any changes are needed.

---
+++ Homework.py
@@ -0,0 +1,21 @@
+a="congrats you won the world cup"
+
+print(a)
+
+print(a[8:])    
+
+print(a.upper())
+
+index=a.find("won")
+
+print("index of won:", index)
+
+index=a.find("lost")
+
+print("index of lost:", index)
+
+input_boolean=input("Enter True or False: ")
+
+loop_boolean=input("Enter True or False: ")
+
+loop=5.55
